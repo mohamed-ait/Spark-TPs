e9cc03bbdc9cc0d7c589f46f1bcd541e6d1fe245
mohamed-ait <mohamedaitlahcen62@gmail.com>
2022-04-19 20:18:15 +0000

    application6

--- rdd-tp1/src/main/java/Exercice3/Application6.java
+++ rdd-tp1/src/main/java/Exercice3/Application6.java
@@ -11,16 +11,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Application5 {
+public class Application6 {
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("Top 5 des stations météo les plus chaudes.").setMaster("local[*]");
+        SparkConf conf = new SparkConf().setAppName("Top 5 des stations météo les plus froides.").setMaster("local[*]");
         JavaSparkContext cxt=new JavaSparkContext(conf);
         JavaRDD<String> rdd1=cxt.textFile("temperatures.csv");
         JavaRDD<String> rdd2=rdd1.filter(f->{
-            return f.split(",")[2].equals("TMAX");
+            return f.split(",")[2].equals("TMIN");
         });
         JavaPairRDD<Integer,String> rdd3=rdd2.mapToPair(f->new Tuple2<>(Integer.parseInt(f.split(",")[3]),f.split(",")[0]));
-        JavaPairRDD<Integer,String> sortedRdd=rdd3.sortByKey(false);
+        JavaPairRDD<Integer,String> sortedRdd=rdd3.sortByKey(true);
         List<Tuple2<Integer,String>> els=sortedRdd.take(5);
         els.forEach(f-> System.out.println(f._2()+"-->"+f._1()));
     }
